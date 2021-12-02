43f7cd6dbfb525208efed59987c142b9cc08be60
sy <shi06yue@gmail.com>
2021-12-02 13:06:45 +0800

    新增ota支持

--- power-meter-8266.ino
+++ power-meter-8266.ino
@@ -1,4 +1,7 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include "TM1637.h"
 
@@ -134,6 +137,44 @@ void setup() {
 	// }
 
 
+	ArduinoOTA.setHostname("power-meter");
+	// ArduinoOTA.setPassword("12345678");
+
+	ArduinoOTA.onStart([]() {
+	String type;
+	if (ArduinoOTA.getCommand() == U_FLASH) {
+		type = "sketch";
+	} else { // U_FS
+		type = "filesystem";
+	}
+
+	// NOTE: if updating FS this would be the place to unmount FS using FS.end()
+	Serial.println("Start updating " + type);
+	});
+	ArduinoOTA.onEnd([]() {
+		Serial.println("\nEnd");
+	});
+	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+		Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+	});
+		ArduinoOTA.onError([](ota_error_t error) {
+		Serial.printf("Error[%u]: ", error);
+		if (error == OTA_AUTH_ERROR) {
+			Serial.println("Auth Failed");
+		} else if (error == OTA_BEGIN_ERROR) {
+			Serial.println("Begin Failed");
+		} else if (error == OTA_CONNECT_ERROR) {
+			Serial.println("Connect Failed");
+		} else if (error == OTA_RECEIVE_ERROR) {
+			Serial.println("Receive Failed");
+		} else if (error == OTA_END_ERROR) {
+			Serial.println("End Failed");
+		}
+	});
+	ArduinoOTA.begin();
+
+
+
 ///////////////////////////////////
 	// Auto Setting
 	// lcd.print("setting...");
@@ -186,8 +227,11 @@ void setup() {
 
 
 void loop(){
+	
 	// yield();
 	while(1){
+		ArduinoOTA.handle();
+
 		light1 = analogRead(A0);
 		if(light1 > powerledPin_average){
 			// led on
@@ -245,6 +289,8 @@ void loop(){
 	
 	// yield();
 	while(1){
+		ArduinoOTA.handle();
+
 		light1 = analogRead(A0);
 		if(light1 > powerledPin_average){
 			// led on
